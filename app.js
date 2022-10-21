46a705ce37ddcf2612c5853e9bb068327c01184a
SoreviR <rob.riveros.es@gmail.com>
2022-10-21 15:47:42 +0200

    Day 47 Add a database encryption for level 2 security for the password

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const { stringify } = require("querystring");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -12,10 +13,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
 
 const User = new mongoose.model("User", userSchema);
 
